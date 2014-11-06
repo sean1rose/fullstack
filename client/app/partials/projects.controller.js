89015125ec91b6593d6a30632e3668d46b111e34
Andrew Teich <andrewteich@me.com>
2014-11-06 11:56:57 -0800

    Appeased jshint

--- client/app/partials/projects.controller.js
+++ client/app/partials/projects.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('fullstackApp')
-	.controller('ProjectsCtrl', function ($scope, $http) {
+	.controller('ProjectsCtrl', function ($scope) {
 		$scope.projects = [1,2];
 		$scope.pMod0 = $scope.projects.length % 3 === 0;
 		$scope.pMod1 = $scope.projects.length % 3 === 1;
