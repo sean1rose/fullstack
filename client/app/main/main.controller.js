bf746e08a1aaaa5f324fcd037bb269bf885b87f6
sean1rose <sean1rose@gmail.com>
2014-11-19 17:38:56 -0800

    Add down arrow to splash, add featured project to browse projects page

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,27 +1,11 @@
 'use strict';
 
 angular.module('fullstackApp')
-  .controller('MainCtrl', function () {
-    // $scope.awesomeThings = [];
-    // $scope.projects = [1,2];
-    // $scope.pMod0 = $scope.projects.length % 3 === 0;
-    // $scope.pMod1 = $scope.projects.length % 3 === 1;
-    // $scope.pEven = $scope.projects.length % 2 === 0;
-    // $scope.pMod2 = $scope.projects.length % 3 === 2;
-
-    // $http.get('/api/projects').success(function(projects) {
-    //   $scope.projects = projects;
-    // });
-
-    // $scope.addThing = function() {
-    //   if($scope.newThing === '') {
-    //     return;
-    //   }
-    //   $http.post('/api/things', { name: $scope.newThing });
-    //   $scope.newThing = '';
-    // };
-
-    // $scope.deleteThing = function(thing) {
-    //   $http.delete('/api/things/' + thing._id);
-    // };
-  });
+  .controller('MainCtrl', ['$scope', '$location', 'anchorSmoothScroll', 
+    function($scope, $location, anchorSmoothScroll){
+        $scope.goto = function(eID){
+            $location.hash('useit');
+            anchorSmoothScroll.scrollTo(eID);
+        };
+    }
+  ]);
