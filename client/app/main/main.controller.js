b1192b159892a9fb77dc04c679493e03698600b4
sean1rose <sean1rose@gmail.com>
2014-11-24 22:26:08 -0800

    Contribute parallax set up, need content

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,9 +3,5 @@
 angular.module('fullstackApp')
   .controller('MainCtrl', ['$scope', '$location', 
     function($scope, $location){
-        // $scope.goto = function(eID){
-        //     $location.hash(eID);
-        //     anchorSmoothScroll.scroll();
-        // };
     }
   ]);
--- client/app/main/main.js
+++ client/app/main/main.js
@@ -8,16 +8,9 @@ angular.module('fullstackApp')
         templateUrl: 'app/main/main.html',
         controller: 'MainCtrl'
       })
-      // See angular parameter documentation here: https://github.com/angular-ui/ui-router/wiki/URL-Routing
-      // this should be separated into own folder
       .state('individualProjects', {
         url: '/projects/:id',
         templateURL: 'app/project/individualprojects.html',
         controller: 'ProjectCtrl'
       });
-      // .state('contribute',{
-      //   url: '/contribute',
-      //   templateUrl: 'app/contribute/contribute.html',
-      //   controller: 'ContributeController'
-      // })
   })
